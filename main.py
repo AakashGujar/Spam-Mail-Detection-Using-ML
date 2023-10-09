aba4f12a157eda6d03e99f7137505cb858b95909
DRAG-11 <bundeledurwang@gmail.com>
2023-10-09 19:04:16 +0530

    fixe issue with detection

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ from sklearn.metrics import accuracy_score
 # processing the data
 raw_mail_data = pd.read_csv('mail_data.csv')
 mail_data = raw_mail_data.where((pd.notnull(raw_mail_data)),'')
-mail_data.loc[mail_data['Category'] == 'spam', Category,] = 0
-mail_data.loc[mail_data['Category'] == 'ham', Category,] = 1
+mail_data.loc[mail_data['Category'] == 'spam', 'Category',] = 0
+mail_data.loc[mail_data['Category'] == 'ham', 'Category',] = 1
 X = mail_data['Message']
 
 Y = mail_data['Category']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=3)
 # transform the text data to feature vectors that can be used as input to the Logistic regression and 
 # converting Y_train and Y_test values as integers
-feature_extraction = TfidfVectorizer(min_df=1, stop_words='english', lowercase="True")
+feature_extraction = TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)
 
 X_train_features = feature_extraction.fit_transform(X_train)
 X_test_features = feature_extraction.transform(X_test)
